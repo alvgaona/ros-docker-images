02c3303f687434a45c2988ece84ce11469bd9715
Alvaro Gaona <alvgaona@gmail.com>
2025-11-09 17:00:36 +0100

    Update ros entrypoints

--- jazzy/ros_entrypoint.sh
+++ jazzy/ros_entrypoint.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 set -e
 
+# Update jazzy user's UID/GID if specified (for volume mounting)
+if [ -n "$HOST_UID" ] && [ -n "$HOST_GID" ]; then
+    echo "Updating jazzy user to UID:GID = $HOST_UID:$HOST_GID"
+
+    # Get current UID/GID
+    CURRENT_UID=$(id -u jazzy)
+    CURRENT_GID=$(id -g jazzy)
+
+    # Update if different
+    if [ "$CURRENT_GID" != "$HOST_GID" ]; then
+        groupmod -g "$HOST_GID" jazzy
+    fi
+    if [ "$CURRENT_UID" != "$HOST_UID" ]; then
+        usermod -u "$HOST_UID" jazzy
+    fi
+
+    # Fix ownership of home directory
+    chown -R jazzy:jazzy /home/jazzy
+fi
+
 # Source ROS 2 setup
 source /opt/ros/jazzy/setup.bash
 
 # Source workspace setup if it exists
-if [ -f /ros2_ws/install/setup.bash ]; then
-    source /ros2_ws/install/setup.bash
+if [ -f /home/jazzy/ros2_ws/install/setup.bash ]; then
+    source /home/jazzy/ros2_ws/install/setup.bash
 fi
 
 exec "$@"
